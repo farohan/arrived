65ba3b7b729711243f1e2be139bc5af67d44c06d
farohan <farohan2019@gmail.com>
2021-07-22 14:04:06 -0700

    images added

---
+++ app.js
@@ -0,0 +1,8 @@
+//These two functions open and close the sidebar
+function openSideBar() {
+    document.getElementById('all-content-div').style.width = "33vw";
+}
+
+function closeSideBar() {
+    document.getElementById('all-content-div').style.width = "0px";
+}
