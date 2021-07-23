7b73c36561eb105502366e43fa7b0bc137bf94b1
farohan <68710283+farohan@users.noreply.github.com>
2021-07-22 19:41:01 -0700

    Create products.js

---
+++ products.js
@@ -0,0 +1,59 @@
+//This is where all the products and their statistics are.
+// They will be stored as JavaScript objects.
+
+/* ______________________________________________________________________________ */
+
+// Home and Garden
+
+let hardwoodTable = {
+    name: 'Hardwood Table',
+    imageURL: 'https://www.smow.com/pics/mu-063-000/muuto-linear-wood-table-h74xl200xb90-01_zoom.jpg',
+    imageAlt: 'Wood table',
+    price: '$39.99',
+    starRating: '',
+    shippingFee: '$6.75'
+};
+
+let tableLamp = {
+    name: 'Lovely Table Lamp 27.5" with Fabric Shade',
+    imageURL: 'https://mobileimages.lowes.com/productimages/c5492ce9-f725-43da-a043-92e8f8796edd/10524199.jpg?size=pdhi',
+    imageAlt: 'A nice table lamp',
+    price: '$34.99',
+    starRating: '',
+    shippingFee: 'Free Shipping'
+};
+
+// Tools and Equipment
+
+// Clothing / Fashion / Jewelry
+
+// Grocery
+
+// Movies + Entertainment
+
+// Parties + Celebrations
+
+// Books
+
+// Electronics
+
+// Sports + Fitness Equipment
+
+// Toys / Baby / Kids
+
+let beachBall = {
+    name: 'Beach Ball for Kids (Multicolored)',
+    imageURL: 'https://target.scene7.com/is/image/Target/GUEST_c6d1bfca-3003-4245-b46e-f7f94cf6908c?wid=488&hei=488&fmt=pjpeg',
+    imageAlt: 'A beach ball',
+    price: '$4.99',
+    starRating: '',
+    shippingFee: 'Free Shipping'
+};
+
+// School + Academics
+
+// Pet Supplies
+
+// Industrial
+
+
