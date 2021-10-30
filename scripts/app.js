79516e9a49b8ce13150eaf50e39ed909ca772101
farohan <68710283+farohan@users.noreply.github.com>
2021-10-30 16:44:57 -0700

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,16 @@
+//Welcome to App.js! 
+//This file is like a handyman. It does all the applicable work in the document.
+
+/*______________________________________________________________________________________*/
+
+//These two functions open and close the sidebar
+const sideMenu = document.getElementById('all-content-div');
+
+function openSideBar() {
+    sideMenu.style.width = "33vw";
+}
+
+function closeSideBar() {
+    sideMenu.style.width = "0px";
+}
 
