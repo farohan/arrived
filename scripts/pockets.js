a252a6a9f5ad4329dce03682d297e3412578b6c2
farohan <68710283+farohan@users.noreply.github.com>
2021-12-19 17:00:17 -0800

    Create pockets.js

---
+++ scripts/pockets.js
@@ -0,0 +1,11 @@
+//Welcome to pockets.js!
+//Here, I count how many empty spaces there are where I could put products.
+
+/* ____________________________________________________________________ */
+
+const pockets = [
+    document.querySelector('#most-bought-item'),
+    document.querySelectorAll('.trending-item'),
+    document.querySelectorAll('.new-addition'),
+    document.querySelectorAll('.product')
+];
